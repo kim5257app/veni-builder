41eb28aec345d4649313550e5d1efd0761a6fc17
kim5257 <kim5257@naver.com>
2022-08-03 01:28:01 +0900

    최초 등록

--- init_data.sh
+++ init_data.sh
@@ -16,3 +16,20 @@ else
     # 설정 파일 복사
     cp ./src/veni-nginx/default.conf /home/master/nfs/veni-nginx/conf.d
 fi
+
+if [ -d "/home/master/nfs/veni-api" ]; then
+    echo "Nginx folder already exist"
+else
+    mkdir /home/master/nfs/veni-api
+    mkdir /home/master/nfs/veni-api/config
+    mkdir /home/master/nfs/veni-api/upload
+fi
+
+if [ -d "/home/master/nfs/veni-db" ]; then
+    echo "Nginx folder already exist"
+else
+    mkdir /home/master/nfs/veni-db
+    mkdir /home/master/nfs/veni-db/data
+    mkdir /home/master/nfs/veni-db/conf
+fi
+
